4678b0b277e5f40f151a7ccd321ae451244f1c09
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2022-08-23 20:43:04 +0100

    Added test copy with logging widget

--- src/wezel/actions/test.py
+++ src/wezel/actions/test.py
@@ -1,16 +1,26 @@
 import wezel
 import time
 
-def testFunction(start, end, signals):
+def test_Function(start, end, signals):
   signals.log.emit("Calculation Started")
   for n in range(start, end):
     time.sleep(1)
     signals.log.emit("Message " + str(n))  
   return "Result of the calculation"
 
+
+def test_Copy(series_list, signals):
+    signals.log.emit("Copying {} series".format(len(series_list)))      
+    for series in series_list:
+        signals.log.emit('Copying {}'.format(series.label()))
+        #series.copy()  
+    return "Copying complete"
+        
+
 def all(parent):
     parent.action(Test_UserInput, text='Test UserInput')
     parent.action(Test_LoggingToGUI, text='Test Logging to GUI')
+    parent.action(Test_Copy, text='Test Copy & Logging to GUI')
     
 
 class Test_LoggingToGUI(wezel.Action):
@@ -18,7 +28,7 @@ class Test_LoggingToGUI(wezel.Action):
         return True
 
     def run(self, app):
-        window = wezel.widgets.LoggingWidget(testFunction, start=1,end=10)
+        window = wezel.widgets.LoggingWidget(test_Function, start=1,end=10)
         app.addAsSubWindow(window, "Test Logging to GUI")
 
 
@@ -47,3 +57,17 @@ class Test_UserInput(wezel.Action):
         if not cancel: 
             for field in input:
                 print(field["label"], ': ', field["value"])
+
+
+class Test_Copy(wezel.Action):
+
+    def enable(self, app):
+        if not hasattr(app, 'folder'):
+            return False
+        return app.nr_selected(3) != 0
+
+    def run(self, app):
+        series_list = app.get_selected(3)
+        window = wezel.widgets.LoggingWidget(test_Copy, series_list=series_list)
+        app.addAsSubWindow(window, "Test Logging to GUI while copying DICOM series")
+       
