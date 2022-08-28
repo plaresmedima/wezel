652584cc140702e0ea1953046ea2bf10e283d732
Steve Shillitoe <44644964+IMI-TRISTAN@users.noreply.github.com>
2022-08-28 20:10:42 +0100

    Update test.py

--- src/wezel/actions/test.py
+++ src/wezel/actions/test.py
@@ -1,5 +1,7 @@
 import wezel
 import time
+import numpy as np
+import os
 
 def test_Function(start, end, signals):
   signals.log.emit("Calculation Started")
@@ -13,15 +15,57 @@ def test_Copy(series_list, signals):
     signals.log.emit("Copying {} series".format(len(series_list)))      
     for series in series_list:
         signals.log.emit('Copying {}'.format(series.label()))
-        #series.copy()  
+        series.copy()  
     return "Copying complete"
+
+
+def test_array_copy(start, end, signals):
+    signals.log.emit("Copying arrays started")      
+    for n in range(start, end):
+        time.sleep(1)
+        first_array=np.arange(n)
+        second_array=np.copy(first_array)
+        signals.log.emit("{} copied".format(first_array)) 
+    return "Array copying complete"
+
+
+def test_file_write(signals):
+    try:
+        if not os.path.exists("C:\\FileDemo\\"):
+            os.makedirs("C:\\FileDemo\\")
+            signals.log.emit("Directory created")
+        with open('C:\\FileDemo\\myfile.txt', 'w+') as f:
+            f.write('Create a new text file!')
+        signals.log.emit("File created") 
+        for n in range(10):
+            time.sleep(1)
+            with open("C:\\FileDemo\\myfile.txt", "a") as f:
+                f.write("\n This text was added using Append.")
+            signals.log.emit("Text appended to file")
+        return "File operations finished"
+    except FileNotFoundError:
+        signals.log.emit("File myfile.txt does not exist")
+        return "Terminated by error"
+
         
 
 def all(parent):
     parent.action(Test_UserInput, text='Test UserInput')
     parent.action(Test_LoggingToGUI, text='Test Logging to GUI')
-    parent.action(Test_Copy, text='Test Copy & Logging to GUI')
+    parent.action(Test_ArrayCopy, text='Test Array Copy & Logging to GUI')
+    parent.action(Test_FileWrite, text='Test Write to File & Logging to GUI')
+    parent.action(Test_Copy, text='Test Copy Series & Logging to GUI')
     
+    
+
+class Test_FileWrite(wezel.Action):
+    def enable(self, app):
+        return True
+
+    def run(self, app):
+        window = wezel.widgets.LoggingWidget(test_file_write)
+        app.addAsSubWindow(window, "Test File Write and Logging to GUI")
+
 
 class Test_LoggingToGUI(wezel.Action):
     def enable(self, app):
@@ -71,3 +115,11 @@ class Test_Copy(wezel.Action):
         window = wezel.widgets.LoggingWidget(test_Copy, series_list=series_list)
         app.addAsSubWindow(window, "Test Logging to GUI while copying DICOM series")
        
+
+class Test_ArrayCopy(wezel.Action):
+    def enable(self, app):
+        return True
+       
+    def run(self, app):
+        window = wezel.widgets.LoggingWidget(test_array_copy, start=10,end=20)
+        app.addAsSubWindow(window, "Test Logging to GUI while copying numpy arrays")
